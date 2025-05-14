d57446cd66c6c699d38d946c9bd1d752cc0ff8ba
feralCode0x <feralcode0x@gmail.com>
2025-05-14 16:39:14 -0400

    update

---
+++ client/build/precache-manifest.6e06d5cbc4d6910021cbfcee36321bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1797f1b45f818d86c878e93c2e4f74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa157ffd821ae9d1ce61",
+    "url": "/static/css/main.b56e9962.chunk.css"
+  },
+  {
+    "revision": "818b8515bc07391b74a4",
+    "url": "/static/js/2.daee5cc9.chunk.js"
+  },
+  {
+    "revision": "b629fb2a09a873276e21b33c798a433f",
+    "url": "/static/js/2.daee5cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa157ffd821ae9d1ce61",
+    "url": "/static/js/main.ae2a0c49.chunk.js"
+  },
+  {
+    "revision": "a65fdc1d42db8039feb9",
+    "url": "/static/js/runtime-main.9b6d1423.js"
+  }
+]);
